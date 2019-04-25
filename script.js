71abfc8f2b51d2ab0299827a74143402995d7a2f
AndrzejWojciechowski0 <wojciechowski969@gmail.com>
2019-04-25 19:58:15 +0200

    aefaefa

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 const api = 'http://pokeapi.co/api/v2'
 
-function buildPokemon(name) {
+function buildPokemon(pokemon) {
     return `
     <div id = "pokedex"><div class = "pokemon row"> 
     <img src = "" class = "img">
     <div class = "col">
-        <span class = "text"> ${name} </span>
-        <span class = "text "> TYP </span>
-        <span class = "text"> WAGA </span>
+        <span class = "text"> ${pokemon.name} </span>
+        <span class = "text "> ${pokemon.types} </span>
+        <span class = "text"> ${pokemon.weight} </span>
     </div>
 </div>
 </div>"
 `
 }
+let pokemons = [];
 function loadPokemons() {
-    fetch(api + "/pokemon")
-        .then(function (resp) {
+    const pro = fetch(api + "/pokemon")
+        pro.then(function (resp) {
             return resp.json();
         })
         .then(function (json) {
-            const listaPokemonow = json.resultrs;
-            const listaHtmliPokemonow = listaPokemonow.map(
-                function (pokemon) { return buildPokemon(pokemon.name); })
+            const listaPokemonow = json.results;
+            let requesty = listaPokemonow.map((pokemon) => {
+                return fetch(pokemon.url)
+                    .then(function (resp) {
+                        return resp.json();
+                    })
+                })
+    Promise.all(requesty).then(function (pokemony){
+            const listaHtmliPokemonow = pokemony
+                .map((pokemon) => buildPokemon(pokemon))
             const htmlWszystkichPokemonow = listaHtmliPokemonow.join();
-            document.querySelector("#pokemony").innerHTML = htmlWszystkichPokemonow
+            document.querySelector("#pokemony").innerHTML = htmlWszystkichPokemonow;
         })
         .catch(function (err) {
             console.error(err)
         })
+    })
 }
 document.addEventListener("load", loadPokemons)
+loadPokemons()
